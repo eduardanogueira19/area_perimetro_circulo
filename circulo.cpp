c78d19838e44d434eb177d6ef56f8885209ede83
eduardanogueira19 <eduardanogueira812@gmail.com>
2023-11-29 15:42:58 -0300

    Create circulo.cpp

---
+++ circulo.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+#define PI 3.14
+
+//área e do perímetro de um círculo.
+int main()
+{
+	float area, perimetro, raio; 
+	
+	printf("Insira o valor do raio:\n");
+	scanf("%f", &raio);
+	
+	perimetro = 2 * PI * raio;
+	area =  PI * pow(raio, 2);
+	printf("Perimetro: %.2f\n", perimetro);
+	printf("Area: %.2f\n", area);
+}
